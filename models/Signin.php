c8e900acefde3194cd1128802d0d670a21c579a0
dananjaya rajamanthri <dananjaya.rajamanthri@gmail.com>
2014-01-23 19:43:27 -0800

    Removed Database from Model

--- models/Signin.php
+++ models/Signin.php
@@ -1,9 +1,10 @@
 <?php
 class Signin extends Model
 {
-
+    private $db;
     public function __construct() {
         parent::__construct();
+        $this->db = new Database(DB_TYPE, DB_HOST, DB_NAME, DB_USER, DB_PASS);
     }
     public function run()
     {
